b66ab466f913b14fdc903e07b3a7b635091d41a0
Petter Rasmussen <petter@hask.no>
2022-08-26 21:26:51 +0300

    Copy assets before asset hash

--- post_build.sh
+++ post_build.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 set -e
 
-env="debug"
+env="$1"
+event="$2"
+target="debug"
 
-if [[ "$1" == "release" ]]; then
-    env="release"
+
+if [[ "$env" == "release" ]]; then
+    target="release"
 fi
 
-# Copy vendor assets
-mkdir -p dist/vendor/ace
-cp htom_web/vendor/ace/worker-html.js dist/vendor/ace
-cp htom_web/vendor/ace/ace.js dist/vendor/ace
-cp htom_web/vendor/ace/mode-html.js dist/vendor/ace
-cp htom_web/vendor/ace/keybinding-vim.js dist/vendor/ace
-cp htom_web/vendor/ace/keybinding-emacs.js dist/vendor/ace
+if [[ "$event" == "before_asset_hash" ]]; then
+    # Copy vendor assets
+    mkdir -p dist/vendor/ace
+    cp htom_web/vendor/ace/worker-html.js dist/vendor/ace
+    cp htom_web/vendor/ace/ace.js dist/vendor/ace
+    cp htom_web/vendor/ace/mode-html.js dist/vendor/ace
+    cp htom_web/vendor/ace/keybinding-vim.js dist/vendor/ace
+    cp htom_web/vendor/ace/keybinding-emacs.js dist/vendor/ace
 
-# Generate html
-./target/$env/htom_cli home_page > dist/index.html
+    if [[ "$env" == "dev" ]]; then
+        # Generate html
+        ./target/$env/htom_cli home_page > dist/index.html
+    fi
+fi
+
+if [[ "$event" == "after_asset_hash" ]]; then
+    # Generate html
+    ./target/$env/htom_cli home_page > dist/index.html
+fi
